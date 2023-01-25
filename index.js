00b61533b2d4e9dae2ae45a571e58863a5aa0ebf
Florian <florianpaulschoene@gmail.com>
2023-01-26 00:03:37 +0100

    finished

--- index.js
+++ index.js
@@ -1,38 +1,64 @@
 const APIKey = "ba838de580d82dd8a358c9ee77591f26";
 const BTNsearch = document.getElementById("btn-search");
-let lat = "";
-let lon = "";
-let weather = "";
 
 BTNsearch.addEventListener("click", async () => {
-	let city = document.getElementById("search").value;
-	await getLocationCoordiantes(city);
-	await getCurrentWeather(lat, lon);
+	const city = document.getElementById("search").value;
+	const { lat, lon } = await getLocationCoordiantes(city);
+	const weather = await getCurrentWeather(lat, lon);
+	const forecast = await getForecast(lat, lon);
 	updateWeather(weather);
+	updateForecast(forecast);
 });
 
 async function getCurrentWeather(lat, lon) {
-	let currentWeatherURL =
-		"https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=metric" + "&appid=" + APIKey;
-	const response = await fetch(currentWeatherURL);
-	const currentWeather = await response.json();
-	console.log(currentWeather);
-	weather = currentWeather;
+	try {
+		const currentWeatherURL =
+			"https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=metric" + "&appid=" + APIKey;
+		const response = await fetch(currentWeatherURL);
+		const currentWeather = await response.json();
+		console.log(currentWeather);
+		return currentWeather;
+	} catch {
+		console.log(error);
+	}
 }
 
 async function getLocationCoordiantes(city) {
-	let locationURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + APIKey;
-	const response = await fetch(locationURL);
-	const locationData = await response.json();
-
-	lat = locationData[0].lat;
-	lon = locationData[0].lon;
-	console.log("lat: " + lat + " // lon: " + lon);
+	try {
+		const locationURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + APIKey;
+		const response = await fetch(locationURL);
+		const locationData = await response.json();
+		return {
+			lat: locationData[0].lat,
+			lon: locationData[0].lon,
+		};
+	} catch {
+		console.log(error);
+	}
 }
 
-const updateWeather = (obj) => {
-	console.log("updatingWeather");
+async function getForecast(lat, lon) {
+	try {
+		const forecastURL =
+			"https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=metric&appid=" + APIKey;
+		const response = await fetch(forecastURL);
+		const forecast = await response.json();
+		return forecast;
+	} catch {
+		console.log(error);
+	}
+}
 
+const getIcon = (icon) => {
+	try {
+		const iconURL = "http://openweathermap.org/img/wn/" + icon + ".png";
+		return iconURL;
+	} catch {
+		console.log(error);
+	}
+};
+
+const updateWeather = (obj) => {
 	// weather-current div
 	const description = document.getElementById("weather-info");
 	const city = document.getElementById("weather-city");
@@ -40,13 +66,20 @@ const updateWeather = (obj) => {
 	const time = document.getElementById("weather-time");
 	const temperature = document.getElementById("weather-temperature");
 	const units = document.getElementById("weather-units");
-	let currentDate = new Date();
+	const icon = document.getElementById("weather-icon");
+	const currentDate = new Date();
+	const month = currentDate.toLocaleString("default", { month: "long" });
 
-	description.textContent = obj.weather[0].description;
+	console.log(month);
+
+	const info = obj.weather[0].description;
+	const uppercaseInfo = info.replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase());
+	description.textContent = uppercaseInfo;
 	city.textContent = obj.name;
-	date.textContent = currentDate.getDate() + " / " + currentDate.getMonth() + 1 + " / " + currentDate.getFullYear();
-	time.textContent = currentDate.getHours() + ":" + currentDate.getMinutes();
-	temperature.textContent = obj.main.temp;
+	date.textContent = currentDate.getDate() + "th " + month + " " + currentDate.getFullYear();
+	time.textContent = currentDate.getHours() + ":" + currentDate.getMinutes() + " Uhr";
+	temperature.textContent = Math.round(obj.main.temp * 10) / 10 + " °C";
+	icon.src = getIcon(obj.weather[0].icon);
 
 	//weather-details div
 	const feelslike = document.getElementById("weather-feelslike-info");
@@ -54,8 +87,62 @@ const updateWeather = (obj) => {
 	const rain = document.getElementById("weather-rain-info");
 	const wind = document.getElementById("weather-wind-info");
 
-	feelslike.textContent = obj.main.feels_like;
-	humidity.textContent = obj.main.humidity;
+	feelslike.textContent = Math.round(obj.main.feels_like * 10) / 10 + " °C";
+	humidity.textContent = obj.main.humidity + " %";
 	// rain.textContent = obj.main.humidity; catch ERROR if not in response!
-	wind.textContent = obj.wind.speed;
+	wind.textContent = obj.wind.speed + " km/h";
 };
+
+const updateForecast = (forecastData) => {
+	const forecastContent = document.querySelector(".weather-forecast");
+	forecastContent.textContent = "";
+
+	for (let i = 0; i < 7; i++) {
+		const forecast = document.createElement("div");
+		forecast.classList = "forecast";
+
+		const forecastDay = document.createElement("div");
+		forecastDay.classList = "forecast-day";
+		forecastDay.textContent = getDayString(i);
+
+		forecast.appendChild(forecastDay);
+
+		const forecastTemp = document.createElement("div");
+		forecastTemp.classList = "forecast-temp";
+		forecastTemp.textContent = Math.round(forecastData.list[i].main.temp * 10) / 10 + " °C";
+		forecast.appendChild(forecastTemp);
+
+		const forecastFeelslike = document.createElement("div");
+		forecastFeelslike.classList = "forecast-feelslike";
+		forecastFeelslike.textContent = Math.round(forecastData.list[i].main.feels_like * 10) / 10 + " °C";
+		forecast.appendChild(forecastFeelslike);
+
+		const forecastSymbol = document.createElement("img");
+		forecastSymbol.classList = "forecast-symbol";
+		let icon = forecastData.list[0].weather[0].icon;
+		forecastSymbol.src = getIcon(icon);
+		forecast.appendChild(forecastSymbol);
+
+		forecastContent.appendChild(forecast);
+	}
+};
+
+const getDayString = (x) => {
+	const today = new Date();
+	let nextDay = new Date();
+	nextDay.setDate(today.getDate() + x);
+	nextDay = nextDay.toLocaleString("en-us", { weekday: "long" });
+
+	return nextDay;
+};
+
+async function init() {
+	const city = "Jena";
+	const { lat, lon } = await getLocationCoordiantes(city);
+	const weather = await getCurrentWeather(lat, lon);
+	const forecast = await getForecast(lat, lon);
+	updateWeather(weather);
+	updateForecast(forecast);
+}
+
+init();
